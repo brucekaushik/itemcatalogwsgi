7eb1584d0cb347e01e6b2b416ecc4b205a8251f2
kaushik laptop <kaushik.srinivas@gmail.com>
2017-04-28 22:38:01 +0530

    database setup file

---
+++ vagrant/catalog/dbsetup.py
@@ -0,0 +1,82 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+ 
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Catalog(Base):
+    __tablename__ = 'catalog'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+       }
+ 
+class Category(Base):
+    __tablename__ = 'category'
+
+
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    catalog_id = Column(Integer,ForeignKey('catalog.id'))
+    catalog = relationship(Catalog)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'id'         : self.id,
+           'name' : self.name,
+           'catalog_id' : self.catalog_id,
+           'user_id' : self.user_id,
+       }
+
+class Item(Base):
+    __tablename__ = 'item'
+
+
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    catalog_id = Column(Integer,ForeignKey('catalog.id'))
+    catalog = relationship(Catalog)
+    category_id = Column(Integer,ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'id'         : self.id,
+           'name' : self.name,
+           'catalog_id' : self.catalog_id,
+           'category_id' : self.category_id,
+           'user_id' : self.user_id,
+       }
+
+engine = create_engine('sqlite:///itemcatalog.db')
+ 
+
+Base.metadata.create_all(engine)
