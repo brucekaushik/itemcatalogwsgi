cbd6dfa75f7866a667947f6c1d31866fc6a3b4ca
kaushik laptop <kaushik.srinivas@gmail.com>
2017-04-29 02:05:24 +0530

    change port (to avoid reconfigure of google+ oauth settings)

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -8,4 +8,4 @@ app.register_blueprint(routes)
 if __name__ == '__main__':
   app.secret_key = 'ItemCatalogUdacity'
   app.debug = True
-  app.run(host = '0.0.0.0', port = 8000)
+  app.run(host = '0.0.0.0', port = 5000)
